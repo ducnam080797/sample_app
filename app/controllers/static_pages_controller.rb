1d1056048ed038f00df1f72ad17f2f85c0041336
ducnam080797 <ducnam0807@gmail.com>
2018-11-22 16:01:36 +0700

    Chapter5

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -4,6 +4,8 @@ class StaticPagesController < ApplicationController
   def help; end
 
   def about; end
-  
+
   def contact; end
+
+  def new; end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,9 @@
 Rails.application.routes.draw do
   scope "(:locale)", locale: /en|vi/ do
-    get "static_pages/contact"
-    get "static_pages/help"
-    get "static_pages/about"
     root "static_pages#home"
+    get "/about", to: "static_pages#about"
+    get "/contact", to: "static_pages#contact"
+    get "/help", to: "static_pages#help"
+    get "/signup", to: "users#new"
   end
 end
