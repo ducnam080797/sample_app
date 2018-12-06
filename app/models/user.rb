70c417420915282b2784d1b99352786a1ab27bff
ducnam080797 <ducnam0807@gmail.com>
2018-12-06 15:25:49 +0700

    chapter7 : Sign_up

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,8 @@
 class User < ApplicationRecord
-  validates :name, presence: true, lenght: { maximum: Settings.user_validate.maximum_name }
+  validates :name, presence: true, length: {maximum: Settings.user_validate.maximum_name}
   VALID_EMAIL_REGEX= /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
-  validates :email, presence: true, lenght: { maximum: Settings.user_validate.minimum_email }, format: { with: VALID_EMAIL_REGEX }, uniqueness: { case_sensitive: false }
+  validates :email, presence: true, length: {maximum: Settings.user_validate.maximum_email}, 
+    format: { with: VALID_EMAIL_REGEX }, uniqueness: { case_sensitive: false }
   has_secure_password
-  validates :password, presence: true, length: { minimum: Settings.user_validate.minimum_password }
+  validates :password, presence: true, length: {maximum: Settings.user_validate.maximum_password}
 end
